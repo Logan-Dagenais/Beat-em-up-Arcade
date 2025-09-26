6b181ff095734426cd5ce510c528c4cd2c5000fa
radical-ish <cruzmatt1015@gmail.com>
2025-09-26 17:40:12 -0500

    very basic enemy spawning system
    it works for the prototype but i am gonna have to refine it a bit more later
    
    i used the portal layer for the trigger for it because we probably don't need to add any more collision layers hopefully

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,12 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] EnemyTypes;
+    [SerializeField] private GameObject[] EnemiesToSpawn;
+    [SerializeField] private float spawnCooldown = 1;
+    [SerializeField] private Vector2 spawnLocationOffset;
+
+    private Transform cam;
+
+    private void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    public void SpawnEnemy(GameObject enemyType)
+    {
+        Instantiate(enemyType, (Vector2)cam.position + spawnLocationOffset, Quaternion.identity);
+    }
 
     public void SpawnEnemy(GameObject enemyType, Vector2 position)
     {
-        Instantiate(enemyType, position, Quaternion.identity);
+        Instantiate (enemyType, position, Quaternion.identity);
     }
 
     public void DebugSpawnMelee()
@@ -23,4 +39,37 @@ public class EnemySpawner : MonoBehaviour
     {
         SpawnEnemy(EnemyTypes[2], Vector2.zero);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        StartCoroutine(SpawnTimer());
+
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    private int index;
+    IEnumerator SpawnTimer()
+    {
+        if(EnemiesToSpawn.Length > 0)
+            SpawnEnemy(EnemiesToSpawn[index]);
+
+        Debug.Log("enemy spawned");
+        yield return new WaitForSeconds(spawnCooldown);
+        if (index < EnemiesToSpawn.Length - 1)
+        {
+            index++;
+            spawnLocationOffset.x *= -1;
+            StartCoroutine(SpawnTimer());
+        }
+        else
+        {
+            StopCoroutine(SpawnTimer());
+        }
+    }
+
 }
