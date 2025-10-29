56be80a70bbc6a176cd8c9ae01514caf7d420b1d
radical-ish <cruzmatt1015@gmail.com>
2025-10-29 18:04:06 -0500

    another camera lock bug fix
    should be no more red errors

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -62,14 +62,13 @@ public class EnemySpawner : MonoBehaviour
             return;
         }
 
-        fightCamCollider.gameObject.SetActive(true);
-
         StartCoroutine(SpawnTimer());
 
         GetComponent<BoxCollider2D>().enabled = false;
 
         if (fightCamCollider)
         {
+            fightCamCollider.gameObject.SetActive(true);
             cinemachineCam.GetComponent<CinemachineConfiner2D>().BoundingShape2D = fightCamCollider; //changes cam to one spot
         }
     }
