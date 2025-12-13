1b961b90be1aaf61a1561a9dd9e0861ac334dfc5
radical-ish <cruzmatt1015@gmail.com>
2025-12-12 18:53:49 -0600

    added healthbar and hit indication
    boss flashes red when hit
    
    there is also now a health bar

--- Assets/Scripts/Characters/BossScript.cs
+++ Assets/Scripts/Characters/BossScript.cs
@@ -1,17 +1,32 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossScript : EnemyScript
 {
+    [SerializeField] private Slider healthbar;
     [SerializeField] private LightningAnim lAnim;
+    [SerializeField] private SpriteRenderer sr;
 
     protected void Start()
     {
         lAnim = transform.GetChild(2).GetComponent<LightningAnim>();
+        sr = GetComponent<SpriteRenderer>();
+        healthbar = GetComponentInChildren<Slider>();
+        healthbar.maxValue = MaxHealth;
+        healthbar.value = Health;
     }
 
     private void Awake()
     {
         base.Awake();
+    }
+
+    public override void TakeDamage()
+    {
+        base.TakeDamage();
+        healthbar.value = Health;
+        StartCoroutine(HitFlash());
     }
 
     protected override void SetBehaviorStateList()
@@ -33,6 +48,14 @@ public class BossScript : EnemyScript
             {(int)BehaviorStates.JUMP,
             new JumpOffenseState(this)}
         };
+    }
+
+    IEnumerator HitFlash()
+    {
+        sr.color = Color.red;
+        yield return new WaitForSeconds(.05f);
+        sr.color = Color.white;
+        StopCoroutine(HitFlash());
     }
 
     public void LightningEffect()
